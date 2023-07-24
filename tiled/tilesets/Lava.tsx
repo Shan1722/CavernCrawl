7cae3ff7b5fa92e6dbbfbb3aeec943f7dff61964
Shan1722 <102873689+Shan1722@users.noreply.github.com>
2023-07-25 00:16:24 +0530

    Add files via upload

---
+++ tiled/tilesets/Lava.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Lava" tilewidth="32" tileheight="32" tilecount="1350" columns="45">
+ <image source="../../../../Users/sk dubey/Downloads/Lava-Cave-Platformer-Level-Tileset4.png" width="1440" height="960"/>
+</tileset>
---
+++ tiled/tilesets/Slime.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Slime" tilewidth="32" tileheight="32" tilecount="80" columns="8">
+ <image source="../../../../Users/sk dubey/Downloads/SlimeLava.png" width="256" height="320"/>
+</tileset>
